061e6f09476c8065e98ed6f1f331b5f52c64bb80
Istvan Kleijn <istvan.kleijn@icr.ac.uk>
2024-12-06 17:42:40 +0000

    3-1: Mull It Over

--- src/03-mull_it_over.py
+++ src/03-mull_it_over.py
@@ -1,8 +1,11 @@
-import pathlib 
+import pathlib
+import re
 
 
-DAY = 0
+DAY = 3
 
+REGEXP_MUL = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
+REGEXP_NUMBER = r"[0-9]{1,3}"
 
 def read_file(day: int):
     file = pathlib.Path("data") / f"{day}.txt"
@@ -11,17 +14,37 @@ def read_file(day: int):
     return(lines)
 
 def parse(lines, *, debug=False):
-    pass
+    pattern_mul = re.compile(REGEXP_MUL)
+    pattern_number = re.compile(REGEXP_NUMBER)
+    mults = list()
+    for line in lines:
+        if debug:
+            print(line)
+        matches = pattern_mul.findall(line)
+        if debug:
+            print(matches)
+        for match in matches:
+            x, y = pattern_number.findall(match)
+            mults.append(Mult(int(x), int(y)))
+    return Object(mults)
+
+class Mult():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def mul(self):
+        return self.x * self.y
 
 class Object:
-    def __init__(self):
-        pass
+    def __init__(self, mults):
+        self.mults = mults
     
     def run_analysis(self, *, debug=False):
-        pass
+        self.sum_of_mults = sum(mult.mul() for mult in self.mults)
     
     def get_answer1(self, *, debug=False):
-        return None
+        return self.sum_of_mults
 
     def get_answer2(self, *, debug=False):
         return None
@@ -32,7 +55,7 @@ class Object:
         return answer1, answer2
 
 test_input = """
-
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 """.strip()
 def test():
     lines = test_input.splitlines()
