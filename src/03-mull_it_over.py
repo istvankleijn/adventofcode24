eb2033a4acdb40ab1fd87960214fafe8549202cd
Istvan Kleijn <istvan.kleijn@icr.ac.uk>
2024-12-09 19:04:04 +0000

    3-2: Mull It Over (unsubmitted)

--- src/03-mull_it_over.py
+++ src/03-mull_it_over.py
@@ -6,6 +6,7 @@ DAY = 3
 
 REGEXP_MUL = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
 REGEXP_NUMBER = r"[0-9]{1,3}"
+REGEXP_DO = r"don\'t.*do"
 
 def read_file(day: int):
     file = pathlib.Path("data") / f"{day}.txt"
@@ -13,19 +14,20 @@ def read_file(day: int):
         lines = handle.readlines()
     return(lines)
 
-def parse(lines, *, debug=False):
-    pattern_mul = re.compile(REGEXP_MUL)
-    pattern_number = re.compile(REGEXP_NUMBER)
-    mults = list()
-    for line in lines:
-        if debug:
-            print(line)
-        matches = pattern_mul.findall(line)
-        if debug:
-            print(matches)
-        for match in matches:
-            x, y = pattern_number.findall(match)
-            mults.append(Mult(int(x), int(y)))
+def parse(lines, *, debug=False, part2=False):
+    mults = list()    
+    text = "\n".join(lines)
+    if part2:
+        pattern_disable = re.compile(REGEXP_DO)
+        text = pattern_disable.sub("", text)
+    if debug:
+        print("Text being analysed: ", text)
+    matches = re.findall(REGEXP_MUL, text)
+    if debug:
+        print("Matching", REGEXP_MUL, "matches found: ", matches)
+    for match in matches:
+        x, y = re.findall(REGEXP_NUMBER, match)
+        mults.append(Mult(int(x), int(y)))
     return Object(mults)
 
 class Mult():
@@ -43,31 +45,33 @@ class Object:
     def run_analysis(self, *, debug=False):
         self.sum_of_mults = sum(mult.mul() for mult in self.mults)
     
-    def get_answer1(self, *, debug=False):
+    def get_answer(self, *, debug=False):
         return self.sum_of_mults
 
-    def get_answer2(self, *, debug=False):
-        return None
-    
-    def get_answers(self, *, debug=False):
-        answer1 = self.get_answer1(debug=debug)
-        answer2 = self.get_answer2(debug=debug)
-        return answer1, answer2
-
-test_input = """
+test1_input = """
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 """.strip()
+test2_input = """
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+""".strip()
 def test():
-    lines = test_input.splitlines()
-    object = parse(lines, debug=True)
-    object.run_analysis(debug=True)
-    test1, test2 = object.get_answers(debug=True)
+    lines1 = test1_input.splitlines()
+    object1 = parse(lines1, debug=True)
+    object1.run_analysis(debug=True)
+    test1 = object1.get_answer(debug=True)
+    lines2 = test2_input.splitlines()
+    object2 = parse(lines2, debug=True, part2=True)
+    object2.run_analysis(debug=True)
+    test2 = object2.get_answer(debug=True)
     print(f"Test 1: {test1}\nTest 2: {test2}")
 
 if __name__ == "__main__":
     test()
     lines = read_file(DAY)
-    object = parse(lines)
-    object.run_analysis()
-    answer1, answer2 = object.get_answers()
+    object1 = parse(lines)
+    object1.run_analysis()
+    answer1 = object1.get_answer()
+    object2 = parse(lines, part2=True)
+    object2.run_analysis()
+    answer2 = object2.get_answer()
     print(f"Answer 1: {answer1}\nAnswer 2: {answer2}")
